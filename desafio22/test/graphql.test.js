773cdec63da0482a55e719a3175221a8d18bc3c4
Juan Bautista Quiroga <juanbaquiroga@gmail.com>
2023-04-21 00:37:33 -0300

    desafio 22

--- desafio22/test/graphql.test.js
+++ desafio22/test/graphql.test.js
@@ -1,32 +1,5 @@
 import axios from "axios";
-// mutation{
-//     createProduct(prodData:{
-//           name: "iPhone 11",
-//           price: 1200,
-//           img: "soyunaimagen.jpg",
-//           stock: 130
-//     }){_id, name, img, stock, price}
-//   }
-  
-//   query{
-//     getProducts{_id, name, price, img, stock}
-//   }
-  
-//   query {
-//     getProduct(id:"6419aec067abf50877de3122"){name, price, _id, stock, img}
-//   }
-  
-//   mutation{
-    // updateProduct(id:"6441a384215b881e1c0a0a29", updateProdData:{name: "iPhone 10",
-    //     img: "soyunaimageneditada.jpg",
-    //     stock: 121,
-    //     price: 1201
-//     } ){_id,name, img, stock, price}
-//   }
-  
-//   mutation{
-//     deleteProduct(id:"6441a384215b881e1c0a0a29"){_id,name, img, stock, price}
-//   }
+
 
 //======================================================================
 //======================= create products ==============================
@@ -115,63 +88,3 @@ const readProductQueryResponse = await axios(readProductQueryOptions);
 
 console.log(readProductsQueryResponse.data);
 console.log(readProductQueryResponse.data);
-
-//======================================================================
-//======================= update products ==============================
-//======================================================================
-console.log(`
-======================================================================
-======================= update products ==============================
-======================================================================
-`)
-
-const updateProductQuery = {
-    query: `
-    mutation {
-        updateProduct(id:${id}, updateProdData:{
-            name: "producto editado",
-            img: "soyunaimageneditada.jpg",
-            stock: 121,
-            price: 1201
-        } ){_id,name, img, stock, price}
-    }
-    `
-};
-
-const updateProductOptions = {
-    url: "http://localhost:3000/graphql",
-    method: "post",
-    data: updateProductQuery,
-};
-
-const updateProductResponse = await axios(updateProductOptions);
-
-console.log(updateProductResponse.data);
-
-
-// ======================================================================
-// ======================= delete products ==============================
-// ======================================================================
-console.log(`
-======================================================================
-======================= delete products ==============================
-======================================================================
-`)
-
-const deleteProductQuery = {
-    query: `
-        mutation{
-                deleteProduct(id:${id}){_id,name, img, stock, price}
-        }`,
-};
-
-const deleteProductOptions = {
-    url: "http://localhost:3000/graphql",
-    method: "POST",
-    data: deleteProductQuery,
-};
-
-const deleteProductResponse = await axios(deleteProductOptions);
-
-console.log(deleteProductResponse.data);
-
